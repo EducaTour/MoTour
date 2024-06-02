aa8fabd1ee98e0393e70768c1c21c77622aa6a62
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-06-02 23:55:37 +0700

    chore: add converter

---
+++ app/src/main/java/com/dicoding/motour/data/db/SringListConverter.kt
@@ -0,0 +1,14 @@
+package com.dicoding.motour.data.db
+import androidx.room.TypeConverter
+
+class StringListConverter {
+    @TypeConverter
+    fun fromString(value: String): List<String> {
+        return value.split(",").map { it }
+    }
+
+    @TypeConverter
+    fun fromList(list: List<String>): String {
+        return list.joinToString(",")
+    }
+}
