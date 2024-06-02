5045630146892c361e00aee4f267a8c0690c9acf
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-06-02 23:56:28 +0700

    chore: dao landmark list

---
+++ app/src/main/java/com/dicoding/motour/data/db/dao/landmark/list/LandmarkDao.kt
@@ -0,0 +1,18 @@
+package com.dicoding.motour.data.db.dao.landmark.list
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.dicoding.motour.data.model.landmark.list.Landmark
+
+@Dao
+interface LandmarkDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(landmark: List<Landmark?>)
+    @Query("DELETE FROM landmark")
+    suspend fun deleteAllLandmark()
+
+    @Query("SELECT * FROM landmark")
+    suspend fun getAllLandmark(): List<Landmark?>
+}
