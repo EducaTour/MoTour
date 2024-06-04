18f45006780791dd74683f6ef1c1b66e8aa6ecc0
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-06-04 23:51:32 +0700

    chore: network DI

---
+++ app/src/main/java/com/dicoding/motour/presentation/di/core/NetModule.kt
@@ -0,0 +1,24 @@
+package com.dicoding.motour.presentation.di.core
+
+import com.dicoding.motour.data.api.EducaTourService
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class NetModule(private val baseUrl: String) {
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .build()
+    }
+
+    fun provideEducaTourService(retrofit: Retrofit): EducaTourService {
+        return retrofit.create(EducaTourService::class.java)
+    }
+}
