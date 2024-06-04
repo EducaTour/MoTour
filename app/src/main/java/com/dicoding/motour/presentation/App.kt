966f41a24e6e430745375c96018d70fe7417cf82
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-06-04 23:49:39 +0700

    chore: add new DI app

---
+++ app/src/main/java/com/dicoding/motour/presentation/App.kt
@@ -0,0 +1,26 @@
+package com.dicoding.motour.presentation
+
+import android.app.Application
+import com.dicoding.motour.presentation.di.Injector
+import com.dicoding.motour.presentation.di.core.AppComponent
+import com.dicoding.motour.presentation.di.core.AppModule
+import com.dicoding.motour.presentation.di.core.NetModule
+import com.dicoding.motour.presentation.di.core.RemoteDataModule
+import com.dicoding.motour.presentation.di.home.HomeSubComponent
+//import com.dicoding.motour.BuildConfig
+
+class App : Application(), Injector {
+    private lateinit var appComponent: AppComponent
+
+    override fun onCreate() {
+        super.onCreate()
+//        appComponent = DaggerAppComponent.builder()
+//            .appModule(AppModule(applicationContext))
+//            .netModule(NetModule(BuildConfig.BASE_URL))
+//            .remoteDataModule(RemoteDataModule())
+//            .build()
+    }
+    override fun createHomeSubComponent(): HomeSubComponent {
+        return appComponent.homeSubComponent().create()
+    }
+}
