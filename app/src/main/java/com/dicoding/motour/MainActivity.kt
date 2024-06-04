c527d7bad8ae4e5f3d35b0ed513729399e5e9af2
skuuzie <138307270+skuuzie@users.noreply.github.com>
2024-06-04 19:15:47 +0700

    feat: scan landmark from camera and gallery

--- app/src/main/java/com/dicoding/motour/MainActivity.kt
+++ app/src/main/java/com/dicoding/motour/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.dicoding.motour
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.dicoding.motour.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,15 +34,50 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNav.itemIconTintList = null
     }
 
-    private fun setupEdgeToEdge() {
-        // TBD: Insets stuff
-    }
-
     private fun setupClickListener() {
         binding.ivScan.setOnClickListener {
-            Intent(this, ScanActivity::class.java).run {
-                startActivity(this)
+            if (!checkPermission(CAMERA_PERMISSION)) {
+                cameraPermissionLauncher.launch(CAMERA_PERMISSION)
+            } else {
+                performScan()
             }
         }
+    }
+
+    private fun performScan() {
+        Intent(this, ScanActivity::class.java).run {
+            startActivity(this)
+        }
+    }
+
+    private fun showNeedCameraPermissionDialog() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(getString(R.string.camera))
+            .setMessage(getString(R.string.need_camera_permission))
+            .setPositiveButton(getString(R.string.close)) { _, _ ->
+                closeOptionsMenu()
+            }
+            .show()
+    }
+
+    private fun checkPermission(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private val cameraPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            performScan()
+        } else {
+            showNeedCameraPermissionDialog()
+        }
+    }
+
+    companion object {
+        private const val CAMERA_PERMISSION = android.Manifest.permission.CAMERA
     }
 }
