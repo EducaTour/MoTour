1a2b61b02335f6d710b1b3fa2152d3a270ae54a5
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-06-05 01:52:48 +0700

    fix: add DB converters and bugs

--- app/src/main/java/com/dicoding/motour/data/db/EducaTourDB.kt
+++ app/src/main/java/com/dicoding/motour/data/db/EducaTourDB.kt
@@ -2,20 +2,24 @@ package com.dicoding.motour.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.dicoding.motour.data.db.dao.landmark.detail.ContactInfoDao
 import com.dicoding.motour.data.db.dao.landmark.detail.LandmarkDetailDao
 import com.dicoding.motour.data.db.dao.landmark.detail.LocationDao
 import com.dicoding.motour.data.db.dao.landmark.detail.OpeningHoursDao
 import com.dicoding.motour.data.db.dao.landmark.detail.TicketPriceDao
+import com.dicoding.motour.data.db.dao.landmark.list.LandmarkDao
 import com.dicoding.motour.data.model.landmark.detail.ContactInfo
 import com.dicoding.motour.data.model.landmark.detail.LandmarkDetail
 import com.dicoding.motour.data.model.landmark.detail.Location
 import com.dicoding.motour.data.model.landmark.detail.OpeningHours
 import com.dicoding.motour.data.model.landmark.detail.TicketPrice
+import com.dicoding.motour.data.model.landmark.list.Landmark
 
 @Database(
     entities = [
         ContactInfo::class,
+        Landmark::class,
         LandmarkDetail::class,
         Location::class,
         OpeningHours::class,
@@ -24,8 +28,9 @@ import com.dicoding.motour.data.model.landmark.detail.TicketPrice
     version = 1,
     exportSchema = false
 )
+@TypeConverters(StringListConverter::class)
 abstract class EducaTourDB : RoomDatabase(){
-    abstract fun landmarkDao(): LandmarkDetailDao
+    abstract fun landmarkDao(): LandmarkDao
     abstract fun landmarkDetailDao(): LandmarkDetailDao
     abstract fun locationDao(): LocationDao
     abstract fun contactInfoDao(): ContactInfoDao
