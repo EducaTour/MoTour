73068c26c3dd734f3f763de103bbee4fc8ff3538
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-06-05 01:53:03 +0700

    chore: fix null safety converter

--- app/src/main/java/com/dicoding/motour/data/db/SringListConverter.kt
+++ app/src/main/java/com/dicoding/motour/data/db/SringListConverter.kt
@@ -1,14 +1,19 @@
 package com.dicoding.motour.data.db
+
 import androidx.room.TypeConverter
+import com.dicoding.motour.data.model.StringList
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class StringListConverter {
     @TypeConverter
-    fun fromString(value: String): List<String> {
-        return value.split(",").map { it }
+    fun fromString(value: String?): StringList? {
+        val listType = object : TypeToken<List<String>>() {}.type
+        return if (value == null) null else StringList(Gson().fromJson(value, listType))
     }
 
     @TypeConverter
-    fun fromList(list: List<String>): String {
-        return list.joinToString(",")
+    fun fromList(list: StringList?): String? {
+        return Gson().toJson(list?.strings)
     }
 }
