9b5bbfd1c9ba67db4b91f7db7268b868f7e4945c
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-06-05 17:14:27 +0700

    chore: add base URL dagger

--- app/src/main/java/com/dicoding/motour/presentation/App.kt
+++ app/src/main/java/com/dicoding/motour/presentation/App.kt
@@ -7,18 +7,19 @@ import com.dicoding.motour.presentation.di.core.AppModule
 import com.dicoding.motour.presentation.di.core.NetModule
 import com.dicoding.motour.presentation.di.core.RemoteDataModule
 import com.dicoding.motour.presentation.di.home.HomeSubComponent
-//import com.dicoding.motour.BuildConfig
+import com.dicoding.motour.BuildConfig
+import com.dicoding.motour.presentation.di.core.DaggerAppComponent
 
 class App : Application(), Injector {
     private lateinit var appComponent: AppComponent
 
     override fun onCreate() {
         super.onCreate()
-//        appComponent = DaggerAppComponent.builder()
-//            .appModule(AppModule(applicationContext))
-//            .netModule(NetModule(BuildConfig.BASE_URL))
-//            .remoteDataModule(RemoteDataModule())
-//            .build()
+        appComponent = DaggerAppComponent.builder()
+            .appModule(AppModule(applicationContext))
+            .netModule(NetModule(BuildConfig.BASE_URL))
+            .remoteDataModule(RemoteDataModule())
+            .build()
     }
     override fun createHomeSubComponent(): HomeSubComponent {
         return appComponent.homeSubComponent().create()
