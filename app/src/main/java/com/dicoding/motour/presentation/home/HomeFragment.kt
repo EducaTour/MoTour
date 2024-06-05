b91c318693d412f409969989978124c605085c7e
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-06-05 17:15:26 +0700

    chore: connect BE with inject

--- app/src/main/java/com/dicoding/motour/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/dicoding/motour/presentation/home/HomeFragment.kt
@@ -1,25 +1,30 @@
 package com.dicoding.motour.presentation.home
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.lifecycle.ViewModelProvider
 import com.dicoding.motour.R
+import com.dicoding.motour.presentation.di.Injector
+import javax.inject.Inject
 
 class HomeFragment : Fragment() {
+    @Inject
+    lateinit var factory: HomeViewModelFactory
+    private lateinit var viewModel: HomeViewModel
 
     companion object {
         fun newInstance() = HomeFragment()
     }
 
-    private val viewModel: HomeViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
+        (requireActivity().application as Injector).createHomeSubComponent().inject(this)
+        viewModel = ViewModelProvider(this, factory)[HomeViewModel::class.java]
     }
 
     override fun onCreateView(
@@ -27,5 +32,18 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getLandmarkList().observe(viewLifecycleOwner) { landmarkList ->
+            Log.i("MYTAG", landmarkList.toString())
+        }
+
+        // Set up button click listener
+        view.findViewById<Button>(R.id.button_example).setOnClickListener {
+            Log.i("MYTAG", "Button was clicked!")
+        }
     }
 }
