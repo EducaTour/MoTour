79454e3c5e36615aff2605ff02543195df89fa73
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-06-05 17:12:17 +0700

    chore: data binding dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.androidx.savedState)
     implementation(libs.androidx.legacy.support.v4)
     implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.databinding.runtime)
     //Annotation processor
     ksp(libs.androidx.anotation)
     // Room
