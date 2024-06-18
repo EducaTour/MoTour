57a6466bff36799707ebbd7cf84a698a69e88d75
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-06-18 22:22:04 +0700

    feat: domain layer usecase scanner

---
+++ app/src/main/java/com/dicoding/motour/domain/usecase/GetScannerUseCase.kt
@@ -0,0 +1,13 @@
+package com.dicoding.motour.domain.usecase
+
+import com.dicoding.motour.data.model.scanner.ScannerResponse
+import com.dicoding.motour.domain.repository.ScannerRepository
+import okhttp3.MultipartBody
+import retrofit2.Response
+import java.io.File
+
+class GetScannerUseCase(private val scannerRepository: ScannerRepository) {
+    suspend fun execute(file: MultipartBody.Part): Response<ScannerResponse> {
+        return scannerRepository.getScanner(file)
+    }
+}
