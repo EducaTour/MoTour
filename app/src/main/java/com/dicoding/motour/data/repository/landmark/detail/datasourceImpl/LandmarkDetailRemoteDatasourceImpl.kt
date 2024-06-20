948233e7274b19e674f2edee5552aac0e5dfb08c
skuuzie <138307270+skuuzie@users.noreply.github.com>
2024-06-20 15:08:35 +0700

    feat: update landmark detail DI for multi-language feature

--- app/src/main/java/com/dicoding/motour/data/repository/landmark/detail/datasourceImpl/LandmarkDetailRemoteDatasourceImpl.kt
+++ app/src/main/java/com/dicoding/motour/data/repository/landmark/detail/datasourceImpl/LandmarkDetailRemoteDatasourceImpl.kt
@@ -2,13 +2,18 @@ package com.dicoding.motour.data.repository.landmark.detail.datasourceImpl
 
 import com.dicoding.motour.data.api.EducaTourService
 import com.dicoding.motour.data.model.landmark.detail.LandmarkDetailData
+import com.dicoding.motour.data.preferences.LanguageUtil
 import com.dicoding.motour.data.repository.landmark.detail.datasource.LandmarkDetailRemoteDatasource
 import retrofit2.Response
 
 class LandmarkDetailRemoteDatasourceImpl(
     private val educaTourService: EducaTourService
 ) : LandmarkDetailRemoteDatasource {
-    override suspend fun getLandmarkDetail(id: Int): Response<LandmarkDetailData> {
-        return educaTourService.getLandmarkDetail(id)
+    override suspend fun getLandmarkDetail(
+        id: Int,
+        language: LanguageUtil.ContentLanguage
+    ): Response<LandmarkDetailData> {
+        val headers = LanguageUtil.getLanguageHeader(language)
+        return educaTourService.getLandmarkDetail(headers, id)
     }
 }
--- app/src/main/java/com/dicoding/motour/presentation/landmark/LandmarkDetailActivity.kt
+++ app/src/main/java/com/dicoding/motour/presentation/landmark/LandmarkDetailActivity.kt
@@ -1,7 +1,9 @@
 package com.dicoding.motour.presentation.landmark
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,8 +11,12 @@ import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
+import com.dicoding.motour.data.model.landmark.detail.LandmarkDetail
 import com.dicoding.motour.databinding.ActivityLandmarkDetailBinding
 import com.dicoding.motour.presentation.di.Injector
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class LandmarkDetailActivity : AppCompatActivity() {
@@ -43,11 +49,34 @@ class LandmarkDetailActivity : AppCompatActivity() {
     }
 
     private fun loadLandmark() {
-        viewModel.getLandmarkById(landmarkId!!).observe(this) {
-            val landmark = it!! // Shouldn't be null at all
 
-            binding.tvLandmarkName.text = landmark.name
-            Log.d("LANDMARK_DETAIL", it.toString())
+        fetchLandmarkData()
+
+        viewModel.landmark.observe(this) {
+            when (it) {
+                is LandmarkDetailViewModel.LandmarkDetailState.Loading -> {
+                    showLoading(true)
+                    showError(false)
+                }
+
+                is LandmarkDetailViewModel.LandmarkDetailState.Result -> {
+                    showLoading(false)
+                    showError(false)
+
+                    val landmark = it.landmark
+
+                    if (landmark != null) {
+                        loadLandmarkData(landmark)
+                    }
+
+                    binding.nestedScrollview.visibility = View.VISIBLE
+                }
+
+                is LandmarkDetailViewModel.LandmarkDetailState.Error -> {
+                    showLoading(false)
+                    showError(true)
+                }
+            }
         }
     }
 
@@ -89,6 +118,80 @@ class LandmarkDetailActivity : AppCompatActivity() {
         binding.fabBack.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+        binding.btnRetry.setOnClickListener {
+            fetchLandmarkData()
+        }
+    }
+
+    private fun fetchLandmarkData() {
+        lifecycleScope.launch {
+            viewModel.getLandmarkById(landmarkId!!)
+        }
+    }
+
+    private fun loadLandmarkData(landmark: LandmarkDetail) {
+        with(binding) {
+            Glide
+                .with(root)
+                .load(landmark.photos[0])
+                .into(ivLandmarkHeader)
+
+            Glide
+                .with(root)
+                .load(landmark.photos[1])
+                .into(ivImageOne)
+
+            tvLandmarkName.text = landmark.name
+            tvLandmarkDescription.text = landmark.description
+            tvLandmarkHistory.text = landmark.history
+
+            tvLandmarkAddress.text = landmark.location?.address ?: "-"
+            tvLandmarkMapUrl.setOnClickListener {
+                val intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(landmark.location!!.locationUrl)
+                )
+                startActivity(intent)
+            }
+
+            tvLandmarkPriceAdult.text = "Rp. ${landmark.ticketPrice?.adult ?: "-"}"
+            tvLandmarkPriceChild.text = "Rp. ${landmark.ticketPrice?.child ?: "-"}"
+
+            tvLandmarkOpenMonday.text = landmark.openingHours?.monday ?: "-"
+            tvLandmarkOpenTuesday.text = landmark.openingHours?.tuesday ?: "-"
+            tvLandmarkOpenWednesday.text = landmark.openingHours?.wednesday ?: "-"
+            tvLandmarkOpenThursday.text = landmark.openingHours?.thursday ?: "-"
+            tvLandmarkOpenFriday.text = landmark.openingHours?.friday ?: "-"
+            tvLandmarkOpenSaturday.text = landmark.openingHours?.saturday ?: "-"
+            tvLandmarkOpenSunday.text = landmark.openingHours?.sunday ?: "-"
+
+            tvLandmarkContactEmail.text = landmark.contactInfo?.email ?: "-"
+            tvLandmarkContactPhone.text = landmark.contactInfo?.phone ?: "-"
+            tvLandmarkContactWebsite.text = landmark.contactInfo?.website ?: "-"
+
+            if (landmark.uniqueActivities.isNotEmpty()) {
+                tvLandmarkUniqueActivities.text = makeBulletedList(landmark.uniqueActivities)
+            } else {
+                tvLandmarkUniqueActivities.text = "-"
+            }
+        }
+    }
+
+    private fun makeBulletedList(items: List<String?>): String {
+        val builder = StringBuilder()
+        for (item in items) {
+            builder.appendLine("• $item")
+        }
+        return builder.toString().trim()
+    }
+
+    private fun showLoading(state: Boolean) {
+        binding.progressIndicator.visibility = if (state) View.VISIBLE else View.GONE
+    }
+
+    private fun showError(state: Boolean) {
+        binding.ivError.visibility = if (state) View.VISIBLE else View.GONE
+        binding.btnRetry.visibility = if (state) View.VISIBLE else View.GONE
     }
 
     companion object {
